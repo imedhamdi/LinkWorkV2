f0fdbf0f7355f6668641ef4f8e82617829ae149e
HAMDI Imed <ihamdi@TORAYFILMS.EU>
2024-08-08 12:23:41 +0200

    URL route login

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.querySelector('#password').value;
 
         try {
-            const response = await fetch('http://127.0.0.1:3000/users/login', {
+            const response = await fetch('https://linkworkv2.onrender.com/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'

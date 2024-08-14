fa4b6e2357b3f24142873bf329bede6fa0be1f22
Imed HAMDI <78560414+imedhamdi@users.noreply.github.com>
2024-08-14 22:15:57 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 const pool = mysql.createPool({
   port:'3306',
-  host:'btdm9da8pq53mylxej9z-mysql.services.clever-cloud.com',
-  user: 'udvymqcdn0405qcm',
-  password:'EFjUmuagCi8J7kMcBZAg',
-  database:'btdm9da8pq53mylxej9z',
+  host:'bk1u3fnuvnsxspeqlgm5-mysql.services.clever-cloud.com',
+  user: 'uaxfnzlhdpfuawbb',
+  password:'ptcloWo2oP0Y1YJqbMnN',
+  database:'bk1u3fnuvnsxspeqlgm5',
   waitForConnections: true,
   connectTimeout: 10000 ,
   queueLimit: 0
